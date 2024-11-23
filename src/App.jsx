9a9f210cb40e165777292d1b4e45166e4eae0d1e
Arunpandian08 <arunpandianda123@example.com>
2024-11-23 13:44:07 +0530

    basic file structures and routing pages and components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import Favorite from './Pages/Favorite/Favorite'
+import Error from './Pages/Error/Error'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorite' element={<Favorite />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/Pages/Favorite/Favorite.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './favorite.css'
+
+const Favorite = () => {
+  return (
+    <div>Favorite</div>
+  )
+}
+
+export default Favorite
